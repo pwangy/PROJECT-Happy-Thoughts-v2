0c70e57c2a272d9904b670a0c615574337b63e7f
Peggy <64912576+pwangy@users.noreply.github.com>
2021-01-13 13:20:38 +0100

    Initial commit

---
+++ code/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { Published } from './components/Published'
+import { NewPost } from './components/NewPost'
+import { Footer } from './components/Footer'
+
+export const App = () => {
+  return (
+    <div className="app-container">
+    <h1>Happy Thoughts Machine <span 
+          role="img"
+          aria-label="love letter emoji"
+          className="h1-span"> {'💌 '}
+        </span></h1>
+      <NewPost />
+      <Published />
+      <Footer />
+    </div>
+  )
+}
